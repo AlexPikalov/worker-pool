b163aa95531c77596e5c594f4d56e5e3ce2b9cf8
Alex Pikalov <alex.pikalov.khar@gmail.com>
2017-10-21 00:37:04 +0200

    Create basic version of Worker Pool

---
+++ index.js
@@ -0,0 +1,34 @@
+function WorkersPool(workers, config = {batchSize: 3}) {
+  let cursor = 0;
+  var runners = workers.map(makeRunner);
+
+  start();
+
+  return Promise.all(runners.map((r) => r.done));
+
+  function makeRunner(fn) {
+  	let run;
+    const promise = new Promise((res) => run = res);
+    const done = promise.then(fn);
+    done.then(runNext).catch(() => {});
+
+    return { run, done };
+  }
+
+  function runNext() {
+    const next = runners[cursor];
+    cursor += 1;
+
+    if (next) {
+      next.run();
+    }
+  }
+
+  function start() {
+    for (; cursor < config.batchSize && cursor < workers.length;) {
+      runNext();
+    }
+  }
+}
+
+module.exports = WorkersPool;
